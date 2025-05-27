2076a71a6d7edca15c43273b568044068e087383
Infoser76-crear <robertorivadineirac@gmail.com>
2025-05-27 16:51:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+const select = document.getElementById('nombre');
+const mensaje = document.getElementById('mainMessage');
+const openBtn = document.getElementById('open');
+const closeBtn = document.getElementById('close');
+const body = document.body;
+const cover = document.getElementById('cover');
+const containerLetter = document.querySelector('.container-letter');
+
+const saludos = {
+  'Vicenta': 'Querida Suegra',
+  'Lenny': 'Querida Cuñada Lenny',
+  'Rosio ': 'Querida Cuñada Rocio',
+  'Karina': 'Querida Cuñada Karina',
+  'Grisel': 'Querida Sobrina Grisel',
+  'Ceci': 'Querida Sobrina Ceci',
+  'Belén': 'Querida Sobrina Belén',
+  'Niche': 'Querida Sobrina Niche',
+  'Gaby': 'Querida Sobrina Gaby'
+};
+
+select.addEventListener('change', () => {
+  const nombre = select.value;
+
+  if (saludos[nombre]) {
+    openBtn.disabled = false;
+
+    const fondos = {
+      'Vicenta': 'linear-gradient(to bottom, #ffe6e6, #ffb3b3)',
+      'Lenny': 'linear-gradient(to bottom, #e6f7ff, #b3ecff)',
+      'Rosio ': 'linear-gradient(to bottom, #ffe6e6, #ffb3b3)',
+      'Karina': 'linear-gradient(to bottom, #e6f7ff, #b3ecff)',
+      'Grisel': 'linear-gradient(to bottom, #e6ffe6, #b3ffb3)',
+      'Ceci': 'linear-gradient(to bottom, #ffe6e6, #ffb3b3)',
+      'Belén': 'linear-gradient(to bottom, #e6f7ff, #b3ecff)',
+      'Niche': 'linear-gradient(to bottom, #ffe6e6, #ffb3b3)',
+      'Gaby': 'linear-gradient(to bottom, #e6f7ff, #b3ecff)'
+    };
+
+    body.style.background = fondos[nombre] || '#ffe4ec';
+
+    mensaje.innerHTML = `
+      ${saludos[nombre]},<br><br>
+      "Hoy celebro tu amor transformador, ese que con manos de alma teje los hilos invisibles de nuestra familia. En ti late la fuerza que nutre, el cuidado que sana y la alegría que convierte lo simple en eterno."
+      Feliz dia de la Madre.
+
+      De parte de Roberto.
+    `;
+  } else {
+    mensaje.innerHTML = `
+      "Hoy celebro tu amor transformador, ese que con manos de alma teje los hilos invisibles de nuestra familia. En ti late la fuerza que nutre, el cuidado que sana y la alegría que convierte lo simple en eterno."
+      Feliz dia de la Madre.
+
+      De parte de Roberto.
+    `;
+    openBtn.disabled = true;
+    body.style.background = '#ffe4ec';
+  }
+});
+
+openBtn.addEventListener('click', () => {
+  cover.classList.add('opened');
+  containerLetter.classList.add('opened'); // Aquí para mostrar el mensaje
+  openBtn.disabled = true;
+  closeBtn.disabled = false;
+  select.disabled = true;
+});
+
+closeBtn.addEventListener('click', () => {
+  cover.classList.remove('opened');
+  containerLetter.classList.remove('opened'); // Aquí para ocultar el mensaje
+  closeBtn.disabled = true;
+  select.disabled = false;
+
+  if (select.value) {
+    openBtn.disabled = false;
+  }
+});
